2315bec1f614fe32a5a4a0fe9f46dc86686fc392
Palakparmar579 <palakparmar114@gmail.com>
2026-03-19 21:47:26 +0530

    remove comments

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("MongoDB Connected:", conn.connection.host);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected ✅");
   } catch (error) {
-    console.error("DB Error:", error.message);
+    console.log("DB Error:", error.message);
     process.exit(1);
   }
 };
