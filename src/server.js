69d645ff6a0199303b2c16d7a6302223c9aae592
Palakparmar579 <palakparmar114@gmail.com>
2026-03-19 19:11:05 +0530

    ready for deployment

--- src/server.js
+++ src/server.js
@@ -32,6 +32,8 @@ app.use("/api/upload",uploadRoutes)
  })
 
 // Step 9: Start Server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
