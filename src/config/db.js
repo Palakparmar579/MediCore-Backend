bc9561534580017d41794a7f665c2b532cf02e5b
Palakparmar579 <palakparmar114@gmail.com>
2026-03-19 13:27:58 +0530

    fix start command for render

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-const connectDB=async ()=>{
-    try{
-          await mongoose.connect("mongodb://localhost:27017/test")
-         console.log("Database connected successfully")
-    }
-    catch(error){
-       console.log(error)
-    }
-}
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected:", conn.connection.host);
+  } catch (error) {
+    console.error("DB Error:", error.message);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
